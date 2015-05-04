2828947f8db1373db28b68e57c205ac4e09d114c
Chris Templin <ctemplin@gmail.com>
2015-05-04 12:15:52 -0400

    Initial commit of Vagrant, Ansible, and Pip configuration files for creating a VM running a default Django application.

---
+++ django/vagrant/vagrant.rb
@@ -0,0 +1,10 @@
+module VagrantConsts
+
+  BOX = "trusty32"
+  DEFAULT_DJANGO_APP_DIRNAME = "webapp"
+  VIRTUALBOX_MACHINE_NAME = "Django"
+  PORT = 8001
+  SYNCED_FOLDER_HOST = ".."
+  SYNCED_FOLDER_GUEST = "/home/vagrant/app"
+
+end
